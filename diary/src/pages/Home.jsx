9d99030659ec06f642c57c4c44e4181170b1101f
JangHyosun <hoys17@gmail.com>
2024-04-10 23:05:54 +0900

    feature: create button Component.

--- diary/src/pages/Home.jsx
+++ diary/src/pages/Home.jsx
@@ -1,10 +1,20 @@
-import { useSearchParams } from "react-router-dom";
+import Button from "../component/Button";
 
 const Home = () => {
-    const [searchParams, setSearchParams] = useSearchParams();
-    console.log(searchParams.get("sort"));
+
     return (
-        <div>Home 페이지입니다.</div>
+        <div>
+            <Button text={"기본 버튼"} onClick={() => {
+                alert("default button");
+            }} />
+            <Button type="positive" text={"긍정 버튼"} onClick={() => {
+                alert("positive button");
+            }} />
+            <Button type="negative" text={"부정 버튼"} onClick={() => {
+                alert("negative button");
+            }} />
+            <h3>Home 페이지입니다.</h3>
+        </div>
     );
 }
 export default Home;
