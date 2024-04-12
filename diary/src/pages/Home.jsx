97a99e94370994330d28c997aacc0a691e81d7d2
JangHyosun <hoys17@gmail.com>
2024-04-12 22:50:49 +0900

    feature : Diarypage write Section, write button.

--- diary/src/pages/Home.jsx
+++ diary/src/pages/Home.jsx
@@ -6,7 +6,9 @@ const Home = () => {
 
     return (
         <div>
-            <Editor />
+            <Editor onSubmit={() => {
+                alert("작성 완료 버튼을 클릭했습니다.")
+            }}/>
             <h3>Home 페이지입니다.</h3>
         </div>
     );
