d79a3281db1f971be7eb7829219e3f0c511d18e5
JangHyosun <hoys17@gmail.com>
2024-04-14 18:41:51 +0900

    feature: Create Diary State.

--- diary/src/App.jsx
+++ diary/src/App.jsx
@@ -1,11 +1,66 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { useReducer, useRef } from "react";
+import { Routes, Route } from "react-router-dom";
 import './App.css';
 import Home from "./pages/Home.jsx";
 import Write from "./pages/Write.jsx";
 import Diary from "./pages/Diary.jsx";
 import Edit from "./pages/Edit.jsx";
 
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE": {
+      return [action.data, ...state];
+    }
+    case "UPDATE": {
+      return state.map((it) =>
+        String(it.id) === String(action.data.id) ? { ...action.data } : it
+    );
+    }
+    case "DELETE": {
+      return state.filter((it) => String(it.id) !== String(action.targetId));
+    }
+    default: {
+      return state;
+    }
+  }
+}
+
 function App() {
+  const [data, dispatch] = useReducer(reducer, []);
+  const idRef = useRef(0);
+
+  const onCreate = (date, content, emotionId) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current,
+        date: new Date(date).getTime(),
+        content,
+        emotionId,
+      }
+    });
+    idRef.current += 1;
+  };
+  
+  const onUpdate = (targetId, date, content, emotionId) => {
+    dispatch({
+      type: "UPDATE",
+      data: {
+        id: targetId,
+        date: new Date(date).getTime(),
+        content,
+        emotionId,
+      },
+    });
+  };
+
+  const onDelete = (targetId) => {
+    dispatch({
+      type: "DELETE",
+      targetId,
+    });
+  };
+
   return (
     <div className="App">
       <Routes>
@@ -14,12 +69,7 @@ function App() {
         <Route path="/diary/:id" element={<Diary />} />
         <Route path="/edit" element={<Edit />} />
       </Routes>
-      <div>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/write"}>New write</Link>
-        <Link to={"/diary"}>Diary</Link>
-        <Link to={"/edit"}>Edit</Link>
-      </div>
+
     </div>
   );
 }
--- diary/src/pages/Home.jsx
+++ diary/src/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
 
     return (
         <div>
-            <Editor
+            {/* <Editor
                 initData={{
                     date: new Date().getTime(),
                     emotionId: 1,
@@ -14,7 +14,7 @@ const Home = () => {
                 }}
                 onSubmit={() => {
                 alert("작성 완료 버튼을 클릭했습니다.")
-            }}/>
+            }}/> */}
             <h3>Home 페이지입니다.</h3>
         </div>
     );
