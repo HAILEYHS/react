218ad6b121520793a0aca460ac7cce32eacfd163
JangHyosun <hoys17@gmail.com>
2024-04-16 23:24:14 +0900

    feature: DiarystateContext

--- diary/src/App.jsx
+++ diary/src/App.jsx
@@ -1,10 +1,12 @@
-import { useEffect, useReducer, useRef } from "react";
+import React, { useEffect, useReducer, useRef, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import './App.css';
 import Home from "./pages/Home.jsx";
 import Write from "./pages/Write.jsx";
 import Diary from "./pages/Diary.jsx";
 import Edit from "./pages/Edit.jsx";
+
+export const DiaryStateContext = React.createContext();
 
 const mockData = [
   {
@@ -51,14 +53,16 @@ function reducer(state, action) {
 function App() {
   const [data, dispatch] = useReducer(reducer, []);
   const idRef = useRef(0);
+  const [isDataLoaded, setIsDataLoaded] = useState(false);
 
   useEffect(() => {
     dispatch({
       type: "INIT",
       data: mockData,
     });
+    setIsDataLoaded(true);
   }, []);
-  
+
   const onCreate = (date, content, emotionId) => {
     dispatch({
       type: "CREATE",
@@ -90,18 +94,22 @@ function App() {
       targetId,
     });
   };
-
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/write" element={<Write />} />
-        <Route path="/diary/:id" element={<Diary />} />
-        <Route path="/edit" element={<Edit />} />
-      </Routes>
-
-    </div>
-  );
+  if (!isDataLoaded) {
+    return <div>데이터를 불러오는 중입니다.</div>;
+  } else {
+    return (
+      <DiaryStateContext.Provider value={data}>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/write" element={<Write />} />
+            <Route path="/diary/:id" element={<Diary />} />
+            <Route path="/edit" element={<Edit />} />
+          </Routes>
+        </div>
+      </DiaryStateContext.Provider>
+    );
+  }
 }
 
 export default App;
