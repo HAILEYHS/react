4498049e7cb5b4d267ce2a5cb27acc38c9104ff2
JangHyosun <hoys17@gmail.com>
2024-04-27 16:17:24 +0900

    feature: Load the image list with the emotion image number emotionId.

--- diary/src/component/Viewer.jsx
+++ diary/src/component/Viewer.jsx
@@ -1,8 +1,13 @@
+import { emotionList } from "../util";
 import "./Viewer.css";
 
 const Viewer = ({ content, emotionId }) => {
+    const emotionItem = emotionList.find((it) => it.id === emotionId);
+    console.log(emotionItem);
     return (
-        <div className="viewer"></div>
+        <div className="viewer">
+            
+        </div>
     );
 };
 export default Viewer;
